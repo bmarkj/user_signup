7f83b15ddb50b489e9fc9f337c419e4e33101590
Mark Brown <mbrown@gmx.us>
2019-01-21 16:18:13 -0600

    completed validation and returning a response

--- main.py
+++ main.py
@@ -5,33 +5,36 @@ import os
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+#main request handler; loads page on initial browser request 
 @app.route("/")
 def index():
     return render_template('signup_form.html')
 
 
-def not_valid_blank(field):
-    if field is None or field.strip():
+#functions to validate user input
+def test_blank(field):
+    if not field.strip() or field is None:
         return True
 
-def not_valid_length(field):
-    if field.len() < 3 or field.len() > 20:
+def test_length(field):
+    if len(field) >= 3 and len(field) <= 20:
         return True
 
-def not_valid_space(field):
-    if ' ' in field:
+def test_space(field):
+    if ' ' not in field:
         return True
 
-def not_valid_match(f1, f2):
-    if f1 != f2:
+def test_match(f1, f2):
+    if f1 == f2:
         return True
 
-def not_valid_char(field):
-    if "@" not in field or "." not in field:
+def test_char(field):
+    if "@" in field and "." in field:
         return True
 
 
-@app.route("/validate-signup", methods=['POST'])
+#request handler to accept user input, test for validity and send response to browser
+@app.route("/validate", methods=['POST'])
 def validate():
     username = request.form['username']
     password = request.form['password']
@@ -40,49 +43,78 @@ def validate():
 
     usr_err_msg = ''
     pwd_err_msg = ''
-    ver_pwd_err = ''
+    ver_err_msg = ''
     eml_err_msg = ''
 
     #validate username
-    if not_valid_blank(username):
-        usr_err_msg = "Username field is required"
-    elif not_valid_length(username):
-        usr_err_msg = "Invalid. Must be between 3 and 20 characters."
-    elif not_valid_space(username):
-        usr_err_msg = "Invalid. Cannot contain spaces."
+    if not test_blank(username):
+        if test_length(username):
+            if test_space(username):
+                usr_err_msg = ''
+                username = username
+            else:
+                usr_err_msg = "Username cannot contain spaces."
+                username = ''
+        else:
+            usr_err_msg = "Username must be between 3 and 20 characters."
+            username = ''
+    else:
+        usr_err_msg = "Username field is required."
+           
+    # #validate password
+    if not test_blank(password):
+        if test_length(password):
+            if test_space(password):
+                pwd_err_msg = ''
+                password = password
+            else:
+                pwd_err_msg = "Password cannot contain spaces."
+                password = ''
+        else:
+            pwd_err_msg = "Password must be between 3 and 30 characters."
+            password = ''
+    else:
+        pwd_err_msg = "Password field is required."
 
-    #validate password
-    if not_valid_blank(password):
-        usr_err_msg = "Password field is required"
-    elif not_valid_length(password):
-        usr_err_msg = "Invalid. Must be between 3 and 20 characters."
-    elif not_valid_space(password):
-        usr_err_msg = "Invalid. Cannot contain spaces."
+    # #validate verify_password
+    if test_match(password, verify_password):
+        ver_err_msg = ''
+        verify_password = verify_password
+    else:
+        ver_err_msg = "Invalid. Passwords do not match"
+        password = ''
+        verify_password = ''
 
-    #validate verify_password
-    if not_valid_match(password, verify_password):
-        ver_pwd_err = "Invalid. Passwords do not match"
-
-    #validate email
-    if  email is not None and email is not email.strip():
-        if not_valid_length(email):
-            eml_err_msg = "Invalid. Must be between 3 and 20 characters."
-        elif not_valid_space(email):
-            eml_err_msg = "Invalid. Cannot contain spaces."
-        elif not_valid_char(email):
-            eml_err_msg = 'Invalid. Must conatin both the "@" and "." characters.'
-
-    return None
-        
-        
-@app.route("/welcome")
-def welcome():
-    pass
-
-
-
-
-
+    # #validate email
+    if not test_blank(email):
+        if test_length(email):
+            if test_space(email):
+                if test_char(email):
+                    eml_err_msg = ''
+                    email = email
+                else:
+                    eml_err_msg = "Email must contain both the '@' and '.' characters."
+                    email = ''
+            else:
+                eml_err_msg = "Email cannot contain spaces."
+                email = ''
+        else:
+            eml_err_msg = "Email must be between 3 and 30 characters."
+            email = ''
+    else:
+        pass
+   
+    #direct output
+    if not usr_err_msg and not pwd_err_msg and not ver_err_msg and not eml_err_msg:
+        return render_template('welcome.html', name=username)
+    else:
+        return render_template('signup_form.html', 
+        usr_err_msg=usr_err_msg,
+        pwd_err_msg=pwd_err_msg,
+        ver_err_msg=ver_err_msg,
+        eml_err_msg=eml_err_msg,
+        username=username,
+        email=email)    
 
 
 app.run()
